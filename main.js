fedb93f86779a2f0276791de1e6d2e86381fc0b9
talmobi <talmo.christian@gmail.com>
2020-01-02 03:26:48 +0200

    bang

---
+++ main.js
@@ -0,0 +1,84 @@
+const puppeteer = require( 'puppeteer' )
+
+const fs = require( 'fs' )
+const path = require( 'path' )
+
+const express = require( 'express' )
+const http = require( 'http' )
+const app = express()
+app.use( express.static( __dirname ) )
+
+const server = http.createServer( app )
+
+// polling sockets
+const kiite = require( 'kiite' )
+const io = kiite( server )
+
+const childProcess = require( 'child_process' )
+// track spawns
+const nozombie = require( 'nozombie' )
+const nz = nozombie()
+
+io.on( 'connect', function ( socket ) {
+  console.log( 'kiite socket connected!' )
+
+  socket.on( 'execute', function ( command ) {
+    console.log( 'socket command execution: ' + command )
+
+    const split = command.trim().split( /\s+/ )
+
+    const cmd = split.shift() // first element is command
+    const args = split // the rest are arguments
+
+    const spawn = childProcess.spawn( cmd, args )
+
+    // pipe output to console
+    spawn.stdout.pipe( process.stdout )
+    spawn.stderr.pipe( process.stderr )
+
+    // time-to-live should exit within 10 seconds
+    const ttl = 1000 * 10
+    nz.add( spawn.pid, ttl )
+  } )
+
+  socket.on( 'disconnect', function ( command ) {
+    console.log( 'kiite socket left.' )
+  } )
+} )
+
+// get random free port
+const getPort = require( 'get-port' )
+
+start()
+
+async function start () {
+  const port = await getPort()
+  server.listen( port, function () {
+    console.log( 'server listening at: ', server.address() )
+    launchPuppeteer( server.address().port )
+  } )
+}
+
+async function launchPuppeteer ( port ) {
+  const opts = {
+    // set to false to see what we're doing during development
+    headless: false
+  }
+
+  // auto allow mic access
+  opts.args = [
+    // enables mic without user dialog prompt
+    '--use-fake-ui-for-media-stream'
+  ]
+
+  const browser = await puppeteer.launch( opts )
+  const page = await browser.newPage()
+
+  const preload = (`
+    console.log( '${ Date.now() }' )
+  `)
+
+  fs.writeFileSync( 'preload.js', preload, 'utf8' )
+
+  await page.goto( `http://127.0.0.1:${ port }` )
+}
